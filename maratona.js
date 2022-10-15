e7425028a8a621e7e3a94e8d1e41084a943160ff
gnramos <ramos@gnramos.com>
2022-10-15 17:26:41 -0300

    Inclusão de estatísticas de participações em finais mundiais.

--- maratona.js
+++ maratona.js
@@ -109,21 +109,18 @@ function drawVisualization(rows) {
     return `<div style="padding:5px 5px 5px 5px; min-width:75px;"><strong>Rank:</strong> ${rank} ${rankImg(year, phase, 12, rank)}</div>`;
   }
 
-  function makeRow(row) {
-    return [row[0], // year
-            row[1], toolTip(row[0], '1aFase', row[1] == null ? '' : row[1]), // 1aFase
-            row[2], toolTip(row[0], 'Nacional', row[2] == null ? '': row[2])];  // Nacional
-  }
-
   google.charts.load('current', {packages: ['corechart', 'line']});
   var data = new google.visualization.DataTable();
   data.addColumn('string', 'Year');
-  data.addColumn('number', '1aFase');
-  data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
-  data.addColumn('number', 'Nacional');
-  data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+  for (let phase of ['1aFase', 'Nacional', 'Mundial']) {
+    data.addColumn('number', phase);
+    data.addColumn({'type': 'string', 'role': 'tooltip', 'p': {'html': true}});
+  }
   for (let row of rows)
-    data.addRow(makeRow(row));
+    data.addRow([row[0], // year
+                 row[1], toolTip(row[0], '1aFase', row[1] == null ? '' : row[1]),
+                 row[2], toolTip(row[0], 'Nacional', row[2] == null ? '': row[2]),
+                 row[3], toolTip(row[0], 'Mundial', row[3] == null ? '': row[3])]);
 
   var options = {hAxis: {title: 'Ano'},
                  vAxis: {title: 'Rank', baseline: 1},
