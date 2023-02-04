6d1796f8448259aa6cf66fdfe447b1df153b4a26
gnramos <ramos@gnramos.com>
2023-02-04 12:33:46 -0300

    Ajustes.

--- docs/maratona.js
+++ docs/maratona.js
@@ -89,8 +89,8 @@ ${phaseLinks}
     <li>
       <span class="dropdown-item">⏴ Escolas</span>
       <ul class="dropdown-menu dropdown-submenu dropdown-submenu-left">
-        <li><a class="dropdown-item" href="${root()}escolas/ne/al/index.html">Alagoas</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/no/ac/index.html">Acre</a></li>
+        <li><a class="dropdown-item" href="${root()}escolas/ne/al/index.html">Alagoas</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/no/am/index.html">Amazonas</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/no/ap/index.html">Amapá</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/ne/ba/index.html">Bahia</a></li>
@@ -104,6 +104,7 @@ ${phaseLinks}
         <li><a class="dropdown-item" href="${root()}escolas/se/mg/index.html">Minas Gerais</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/no/pa/index.html">Pará</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/ne/pb/index.html">Paraíba</a></li>
+        <li><a class="dropdown-item" href="${root()}escolas/su/pr/index.html">Paraná</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/ne/pe/index.html">Pernambuco</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/ne/pi/index.html">Piauí</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/se/rj/index.html">Rio de Janeiro</a></li>
@@ -111,7 +112,6 @@ ${phaseLinks}
         <li><a class="dropdown-item" href="${root()}escolas/su/rs/index.html">Rio Grande do Sul</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/no/ro/index.html">Rondônia</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/no/rr/index.html">Roraima</a></li>
-        <li><a class="dropdown-item" href="${root()}escolas/su/pr/index.html">Paraná</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/su/sc/index.html">Santa Catarina</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/se/sp/index.html">São Paulo</a></li>
         <li><a class="dropdown-item" href="${root()}escolas/ne/se/index.html">Sergipe</a></li>
@@ -130,20 +130,16 @@ ${phaseLinks}
   let socialMediaItems = `
 <li class="nav-item">
   <a href="https://www.facebook.com/maratona/" class="nav-link">
-    <i class="bi bi-facebook" fill="currentColor">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-      </svg>
-    </i>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+      <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+    </svg>
   </a>
 </li>
 <li class="nav-item">
   <a href="https://www.youtube.com/channel/UCuLfhw7dJoKYbzBktNRgAkA" class="nav-link">
-    <i class="bi bi-youtube" fill="currentColor">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
-        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
-      </svg>
-    </i>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+      <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+    </svg>
   </a>
 </li>`;
 
